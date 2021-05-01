fffef74e78d82bcb65077b2ba9ccfc8994b9b553
nbgh46 <nbgh46@naver.com>
2021-05-01 15:25:00 +0900

    네이버 영화 사이트 크롤링 준비

--- week03/scrap.py
+++ week03/scrap.py
@@ -1,6 +1,9 @@
 import requests
 import pprint
 import secret
+import os
+import sys
+import urllib.request
 
 # HTTP GET request
 response = requests.get('http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
@@ -31,4 +34,20 @@ response = requests.get(
 
 pprint.pprint(response.json())
 
+# 파파고 번역 api 활용
 
+# 번역할 문자
+
+# text = input('번역할 문자를 입력하세요 : ')
+# encText = urllib.parse.quote(text)
+# papago_data = 'source=ko&target=en&text=' + encText
+data = {'source': 'ko', 'target': 'en', 'text': '안녕하세요 바보 멍텅구리 '}
+papago_url = 'https://openapi.naver.com/v1/papago/n2mt'
+papago_response = requests.post(
+    papago_url,
+    headers=headers,
+    data=data,
+)
+# print(papago_response.json())
+papago_result = papago_response.json()
+print(papago_result['message']['result']['translatedText'])
